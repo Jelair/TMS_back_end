2d70e57feb002242e376c3a6fceca73f1d388ec9
simplefly <1059229782@qq.com>
2018-02-16 20:29:39 +0800

    apis, models, config, coreweb

--- async_www/app.py
+++ async_www/app.py
@@ -125,9 +125,6 @@ def response_factory(app, handler):
         return resp
     return response
 
-
-
-
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
@@ -145,9 +142,11 @@ def datetime_filter(t):
 def init(loop):
     yield from orm.create_pool(loop=loop, **configs.db)
     app = web.Application(loop=loop, middlewares=[
-
+        logger_factory, auth_factory, response_factory
     ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
+    add_routes(app, 'handlers')
+    add_static(app)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
